7ca8198f6221ad84f41bc8313da6f222407e14fa
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-02-28 20:29:11 -0700

    final touch ups pre-deployment

---
+++ src/components/SkeletonCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="bg-gray-800 p-6 rounded-xl shadow-lg animate-pulse flex flex-col h-full w-full"
+  >
+    <div class="h-[250px] w-[400px] bg-gray-700 rounded-lg mb-4"></div>
+
+    <div class="flex flex-col flex-grow justify-between">
+      <div class="flex flex-col gap-2">
+        <div class="h-6 w-3/4 bg-gray-700 rounded mb-2"></div>
+        <div class="h-4 w-1/2 bg-gray-700 rounded mb-4"></div>
+      </div>
+
+      <div class="flex justify-between mt-4">
+        <div class="h-10 w-24 bg-gray-700 rounded"></div>
+        <div class="h-10 w-24 bg-gray-700 rounded"></div>
+      </div>
+    </div>
+  </div>
+</template>
