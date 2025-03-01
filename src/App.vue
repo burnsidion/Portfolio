eb122e3c51eb59f7c4d43b9e13343feb9d8a5028
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-02-28 18:46:57 -0700

    initial set up for vue and tailwind

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+  <div
+    class="min-h-screen flex items-center justify-center bg-gray-900 text-white text-3xl"
+  >
+    🚀 Tailwind v4 is working!
+  </div>
+</template>
+
+<style scoped>
+</style>
