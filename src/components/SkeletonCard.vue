460dca02bc189c0a47cf940a3fc84c8a1c7f2b76
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-06 02:46:13 -0700

    inspiraUI revamp

--- src/components/SkeletonCard.vue
+++ src/components/SkeletonCard.vue
@@ -1,19 +1,15 @@
 <template>
-  <div
-    class="bg-[#2c2f3b] p-6 rounded-xl shadow-lg animate-pulse flex flex-col h-[600px] w-full max-w-md md:max-w-lg"
-  >
-    <div class="h-[250px] w-full bg-gray-700 rounded-lg mb-4"></div>
+  <div class="bg-[#2c2f3b] p-4 rounded-2xl shadow-lg animate-pulse flex flex-col h-[600px] w-full max-w-md md:max-w-lg">
+    <!-- Placeholder for Video -->
+    <div class="h-[85%] w-full bg-gray-700 rounded-2xl shadow-md mb-4"></div>
 
-    <div class="flex flex-col flex-grow justify-between">
-      <div class="flex flex-col gap-2">
-        <div class="h-6 w-3/4 bg-gray-700 rounded mb-2"></div>
-        <div class="h-4 w-1/2 bg-gray-700 rounded mb-4"></div>
-      </div>
+    <!-- Placeholder for Title -->
+    <div class="h-6 w-3/4 bg-gray-700 rounded mb-2"></div>
 
-      <div class="flex justify-between mt-4">
-        <div class="h-10 w-24 bg-gray-700 rounded"></div>
-        <div class="h-10 w-24 bg-gray-700 rounded"></div>
-      </div>
+    <!-- Placeholder for Buttons -->
+    <div class="flex justify-between mt-4">
+      <div class="h-10 w-24 bg-gray-700 rounded"></div>
+      <div class="h-10 w-24 bg-gray-700 rounded"></div>
     </div>
   </div>
 </template>
