3968ad0a2bfca83e09bd6fd05d9739c131836a9c
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-06 16:53:26 -0700

    refactoring nav bar and bio section, tweaks to aurora background

--- src/components/AuroraBackground.vue
+++ src/components/AuroraBackground.vue
@@ -6,7 +6,7 @@
         )">
             <div class="absolute inset-0 overflow-hidden">
                 <div :class="cn(
-                    'filter blur-[10px] invert dark:invert-0 pointer-events-none absolute -inset-[10px] opacity-50 will-change-transform;',
+                    'filter blur-[10px] invert dark:invert-0 pointer-events-none absolute -inset-[400px] opacity-50 will-change-transform;',
                     '[--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]',
                     '[--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]',
                     '[--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]',
