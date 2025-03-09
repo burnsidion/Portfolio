266cd6842bd657e03863ab2af547bcc1d23e4115
ian.burnside89@gmail.com <ian.burnside89@gmail.com>
2025-03-08 21:47:08 -0700

    weird chrome caching issue fix maybe

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    headers: {
+      'Cache-Control': 'no-cache, must-revalidate, max-age=0',
+    },
+  },
   resolve: {
     extensions: ['.js', '.ts', '.vue'],
   },
